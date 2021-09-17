9ee6f4f966c7d7af5c05e128cbe293d9ed4c01ff
AdelieC <castel.adelie@yahoo.com>
2021-09-17 12:12:16 +0200

    Just a simple input to enter isbn or ean and result is displayed in console

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import {useForm} from 'react-hook-form'
 
-function App() {
+
+const App = () => {
+  const isbn = require('node-isbn')
+  let bookFound
+  const { register, handleSubmit } = useForm({
+    defaultValues: {
+      isbn: '2765410057'
+    },
+  });
+  const onSubmit = (data) => {
+      isbn.resolve(data.isbn).then((book) => {
+        bookFound = book;
+        console.log(bookFound)
+      }).catch(function (err) {
+          console.log('Book not found', err);
+      });
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label>Entrez un isbn : <input type="text" id="isbn" name="isbn" {...register('isbn')} /></label>
+        <input type="submit" value="Tester" />
+      </form>
     </div>
+    
   );
 }
 
